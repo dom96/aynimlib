d264b71afc2a6abd1a15e0c002a80186e35649d0
Dominik Picheta <dominikpicheta@gmail.com>
2018-03-03 21:38:36 +0000

    Cleaned things up. Wrapped more procedures.

--- pcap.nim
+++ pcap.nim
@@ -3,59 +3,68 @@ import posix
 when defined(macosx):
   const
     libName = "libpcap.dylib"
-    ifName = "en0"
+    defaultIfName* = "en0"
 else:
   const
     libName = "libpcap.so"
-    ifName = "eth0"
+    defaultIfName* = "eth0"
 
 type
-  pcap_t* {.importc: "pcap_t", header: "<pcap/pcap.h>".} = distinct pointer
-  pcap_pkthdr* {.importc: "struct pcap_pkthdr", header: "<pcap/pcap.h>", final, pure.} = object
+  pcap_t* {.importc: "pcap_t", header: "<pcap/pcap.h>".} = ptr object
+  pcap_pkthdr* {.importc: "struct pcap_pkthdr", header: "<pcap/pcap.h>",
+                 final, pure.} = object
     ts*: Timeval
     caplen*: uint32
     len*: uint32
-  ppcap_pkthdr* = ref pcap_pkthdr
 
+const PCAP_ERRBUF_SIZE* = 256
 
-proc pcap_open_live*(dev: cstring, snaplen: cint, promisc: cint, to_ms: cint, errbuf: pointer): pcap_t 
-  {.cdecl, dynlib: libName, importc, discardable .}
+# https://github.com/the-tcpdump-group/libpcap/blob/master/pcap-bpf.c#L234
+const
+  DLT_PRISM_HEADER* = 119
+  DLT_AIRONET_HEADER* = 120
+  DLT_IEEE802_11_RADIO* = 127
+  DLT_IEEE802_11_RADIO_AVS* = 163
+
+proc pcap_open_live*(dev: cstring, snaplen: cint, promisc: cint,
+                     to_ms: cint, errbuf: pointer): pcap_t
+  {.cdecl, dynlib: libName, importc.}
 
 proc pcap_create*(source: cstring, errbuf: pointer): pcap_t 
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 proc pcap_activate*(pcap: pcap_t): cint 
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 
 # pcap_findalldevs
 # pcap_freealldevs
 # pcap_lookupdev
 
 proc pcap_open_offline*(fname: cstring, errbuf: pointer): pcap_t
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 
 # pcap_fopen_offline
 # pcap_open_dead
 
 proc pcap_close*(pcap: pcap_t)
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 
 proc pcap_set_snaplen*(pcap: pcap_t, snaplen: cint): cint 
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 proc pcap_snapshot*(pcap: pcap_t): cint
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 
 proc pcap_set_promisc*(pcap: pcap_t, promisc: cint): cint 
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 proc pcap_set_rfmon*(pcap: pcap_t, rfmon: cint): cint 
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 proc pcap_can_set_rfmon*(pcap: pcap_t): cint
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 
 proc pcap_set_timeout*(pcap: pcap_t, to_ms: cint): cint 
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 
 proc pcap_set_buffer_size*(pcap: pcap_t, buffer_size: cint): cint
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 
 # pcap_set_tstamp_type
 # pcap_list_tstamp_types
@@ -72,37 +81,44 @@ proc pcap_set_buffer_size*(pcap: pcap_t, buffer_size: cint): cint
 # pcap_dispatch
 # pcap_loop
 
-proc pcap_next*(pcap: pcap_t, ph: pointer): cstring
-  {.cdecl, dynlib: libName, importc, discardable .}
+proc pcap_next*(pcap: pcap_t, h: ptr pcap_pkthdr): cstring
+  {.cdecl, dynlib: libName, importc.}
+
+proc pcap_next_ex*(pcap: pcap_t, pkt_header: ptr ptr pcap_pkthdr,
+                   pkt_data: ptr cstring): cint
+  {.cdecl, dynlib: libName, importc.}
 
 # pcap_next_ex
 # pcap_breakloop
 
 proc pcap_setnonblock*(pcap: pcap_t, nonblock: cint, errbuf: pointer): cint
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 
 proc pcap_getnonblock*(pcap: pcap_t, errbuf: pointer): cint
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 
 proc pcap_get_selectable_fd*(pcap: pcap_t): cint
-  {.cdecl, dynlib: libName, importc, discardable .}
+  {.cdecl, dynlib: libName, importc.}
 
+proc pcap_set_datalink*(pcap: pcap_t, val: cint): cint
+  {.cdecl, dynlib: libName, importc.}
 
-proc valid*(pcap: pcap_t): bool =
-  return (pcap.pointer != nil)
+proc pcap_geterr*(pcap: pcap_t): cstring
+  {.cdecl, dynlib: libName, importc.}
+
+proc checkError*(pcap: pcap_t, ret: cint) =
+  if ret < 0:
+    raise newException(OSError, $pcap_geterr(pcap))
 
 when isMainModule:
-  var err = newString(256)
-  err = "aaaa"
-  # var p = pcap_open_live(ifName, 1500.cint, 1.cint, 5000.cint, addr(err))
-  var p = pcap_create(ifName, addr(err))
-  echo(repr(p), err)
+  var err = newString(PCAP_ERRBUF_SIZE)
+  var p = pcap_open_live(defaultIfName, 65536.cint, 1.cint, 1.cint, addr(err))
 
-  if valid(p):
+  if not p.isNil():
     # pcap_set_buffer_size(p, 1500.cint)
     # p.pcap_set_nonblock(1, nil)
-    p.pcap_set_timeout(1000.cint)
-    pcap_activate(p)
+    p.checkError p.pcap_set_timeout(1000.cint)
+    p.checkError pcap_activate(p)
     var ph: pcap_pkthdr
     var data = pcap_next(p, addr(ph))
     if data != nil:
